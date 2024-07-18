7eddf34704d22d60daf002656a038680b85daebe
kimtaehan11 <kimtaehan11@gmail.com>
2024-07-19 08:04:04 +0900

    feat: map 관련 정보 설정

--- src/main/java/developx/langrisser/web/MatchController.java
+++ src/main/java/developx/langrisser/web/MatchController.java
@@ -1,16 +1,16 @@
 package developx.langrisser.web;
 
+import developx.langrisser.match.Stage;
+import developx.langrisser.match.WinOrLose;
 import developx.langrisser.match.service.MatchCreator;
 import developx.langrisser.user.User;
 import developx.langrisser.user.service.UserFinder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,11 +21,29 @@ import java.util.List;
 public class MatchController {
 
     private final MatchCreator matchCreator;
+    private final UserFinder userFinder;
 
+    @Transactional
     @GetMapping("{userId}")
     public String proposals(Model model, @PathVariable(name = "userId") Long userId) {
 
+        User user = userFinder.findById(userId);
+        model.addAttribute("user", user);
+
         // username like 검색으로 조회합니다.
         return "match/match";
+    }
+
+    /**
+     * enum
+     */
+    @ModelAttribute("stages")
+    public Stage[] stages() {
+        return Stage.values();
+    }
+
+    @ModelAttribute("winOrLoses")
+    public WinOrLose[] winOrLoses() {
+        return WinOrLose.values();
     }
 }
